bb4322e88bf64217c35c3df89b6f4c8ab5b47357
dal ghale <ghaledalraj1127@gmail.com>
2024-04-21 21:54:05 -0400

    added file dalghale

---
+++ people/michaelp/dalghale/me.js
@@ -0,0 +1,6 @@
+const buttonFavorites = document.getElementById("favorites");
+buttonFavorites.addEventListener("click", sendFavorites);
+
+function sendFavorites(){
+    alert("My family, watching movies, anime and travelling, music, gym, coffee. " )
+}
